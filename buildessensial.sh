9c09fca9da67f135aad180104a2b25ce0937e990
ydner <76888490+ydner@users.noreply.github.com>
2021-01-23 17:25:27 +0700

    Add files via upload

---
+++ buildessensial.sh
@@ -0,0 +1,1 @@
+apt update && apt upgrade && apt install nano bc bison ca-certificates curl flex gcc git libc6-dev libssl-dev openssl python ssh wget zip zstd sudo make clang gcc-arm-linux-gnueabi software-properties-common
